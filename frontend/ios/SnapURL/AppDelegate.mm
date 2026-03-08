f6190bc6ada9a665bd41da74d19a03232068ac29
안문주 <ahnmunju@gmail.com>
2026-03-08 23:22:08 +0900

    Adjust iOS release build settings

--- frontend/ios/SnapURL/AppDelegate.mm
+++ frontend/ios/SnapURL/AppDelegate.mm
@@ -1,14 +1,18 @@
 #import "AppDelegate.h"
 
+#if DEBUG
 #import <EXDevLauncher/EXDevLauncherController.h>
+#endif
 #import <React/RCTBridge.h>
 #import <React/RCTBridge+Private.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 #import <React/RCTRootView.h>
 
+#if DEBUG
 @interface AppDelegate () <EXDevLauncherControllerDelegate>
 @end
+#endif
 
 @implementation AppDelegate
 
@@ -16,15 +20,23 @@
 {
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 
+#if DEBUG
   EXDevLauncherController *controller = [EXDevLauncherController sharedInstance];
   [controller startWithWindow:self.window delegate:self launchOptions:launchOptions];
+#else
+  [self initializeReactNativeApp];
+#endif
 
   return YES;
 }
 
 - (RCTBridge *)initializeReactNativeApp
 {
+#if DEBUG
   NSDictionary *launchOptions = [EXDevLauncherController.sharedInstance getLaunchOptions];
+#else
+  NSDictionary *launchOptions = nil;
+#endif
   self.bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:self.bridge moduleName:@"main" initialProperties:@{}];
   rootView.backgroundColor = [UIColor systemBackgroundColor];
@@ -54,9 +66,11 @@
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
 {
+#if DEBUG
   if ([EXDevLauncherController.sharedInstance onDeepLink:url options:options]) {
     return YES;
   }
+#endif
 
   return [RCTLinkingManager application:application openURL:url options:options];
 }
@@ -70,12 +84,14 @@ continueUserActivity:(NSUserActivity *)userActivity
                      restorationHandler:restorationHandler];
 }
 
+#if DEBUG
 - (void)devLauncherController:(EXDevLauncherController *)developmentClientController
           didStartWithSuccess:(BOOL)success
 {
   [self initializeReactNativeApp];
   developmentClientController.appBridge = self.bridge.batchedBridge;
 }
+#endif
 
 - (BOOL)isReactInstanceValid
 {
